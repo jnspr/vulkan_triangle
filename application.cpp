f8929152f91b9076e4ac087d8e6c462a42e60d9e
Jonas Sprenger <jnspr@outlook.de>
2023-08-23 23:26:04 +0200

    application.cpp: Implement createVulkanWindow

--- application.cpp
+++ application.cpp
@@ -2,7 +2,7 @@
 
 Application::Application():
     m_glfw(glfw::init()),
-    m_window(1280, 720, "vulkan_triangle"),
+    m_window(createVulkanWindow(1280, 720, "vulkan_triangle")),
     m_graphics(m_window)
 {
 }
@@ -11,4 +11,12 @@ void Application::runUntilClose() {
     while (!m_window.shouldClose()) {
         glfw::pollEvents();
     }
+}
+
+glfw::Window Application::createVulkanWindow(int width, int height, const char *title) {
+    // To support Vulkan, OpenGL must be disabled before window creation
+    glfw::WindowHints hints = {};
+    hints.clientApi = glfw::ClientApi::None;
+    hints.apply();
+    return glfw::Window(width, height, title);
 }
