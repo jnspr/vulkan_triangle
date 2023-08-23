1c77e1d47dce9a53894f824b7ada43efc364b767
Jonas Sprenger <jnspr@outlook.de>
2023-08-23 22:50:16 +0200

    application.hpp: Include glfwpp and add display to Application

--- application.hpp
+++ application.hpp
@@ -1,4 +1,14 @@
 #pragma once
 
+#include <utility>
+#include <glfwpp/glfwpp.h>
+
 class Application {
+public:
+    Application();
+
+    void runUntilClose();
+private:
+    glfw::GlfwLibrary m_glfw;
+    glfw::Window      m_window;
 };
