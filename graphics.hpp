2bd33430db7a910bc3c38e3faaa9088418f7aa61
Jonas Sprenger <jnspr@outlook.de>
2023-08-24 02:22:59 +0200

    graphics.hpp: Add vector of image views

--- graphics.hpp
+++ graphics.hpp
@@ -1,21 +1,23 @@
 #pragma once
 
-#include <vulkan/vulkan.hpp>
+#include <vector>
 #include <utility>
+#include <vulkan/vulkan.hpp>
 #include <glfwpp/glfwpp.h>
 
 class Graphics {
 public:
     explicit Graphics(glfw::Window &window);
 private:
-    vk::UniqueInstance     m_instance;
-    vk::UniqueSurfaceKHR   m_surface;
-    vk::PhysicalDevice     m_physicalDevice;
-    vk::SurfaceFormatKHR   m_surfaceFormat;
-    uint32_t               m_queueFamilyIndex;
-    vk::UniqueDevice       m_logicalDevice;
-    vk::Queue              m_queue;
-    vk::UniqueSwapchainKHR m_swapchain;
+    vk::UniqueInstance               m_instance;
+    vk::UniqueSurfaceKHR             m_surface;
+    vk::PhysicalDevice               m_physicalDevice;
+    vk::SurfaceFormatKHR             m_surfaceFormat;
+    uint32_t                         m_queueFamilyIndex;
+    vk::UniqueDevice                 m_logicalDevice;
+    vk::Queue                        m_queue;
+    vk::UniqueSwapchainKHR           m_swapchain;
+    std::vector<vk::UniqueImageView> m_imageViews;
 
     // Device and presentation setup
     void selectPhysicalDevice();
