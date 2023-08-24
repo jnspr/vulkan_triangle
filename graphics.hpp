20db87e92ddd2daba2c4ab9f4e0a0e24fe2e878f
Jonas Sprenger <jnspr@outlook.de>
2023-08-24 02:00:42 +0200

    graphics.hpp: Make swapchain unique

--- graphics.hpp
+++ graphics.hpp
@@ -8,14 +8,14 @@ class Graphics {
 public:
     explicit Graphics(glfw::Window &window);
 private:
-    vk::UniqueInstance   m_instance;
-    vk::UniqueSurfaceKHR m_surface;
-    vk::PhysicalDevice   m_physicalDevice;
-    vk::SurfaceFormatKHR m_surfaceFormat;
-    uint32_t             m_queueFamilyIndex;
-    vk::UniqueDevice     m_logicalDevice;
-    vk::Queue            m_queue;
-    vk::SwapchainKHR     m_swapchain;
+    vk::UniqueInstance     m_instance;
+    vk::UniqueSurfaceKHR   m_surface;
+    vk::PhysicalDevice     m_physicalDevice;
+    vk::SurfaceFormatKHR   m_surfaceFormat;
+    uint32_t               m_queueFamilyIndex;
+    vk::UniqueDevice       m_logicalDevice;
+    vk::Queue              m_queue;
+    vk::UniqueSwapchainKHR m_swapchain;
 
     // Device and presentation setup
     void selectPhysicalDevice();
