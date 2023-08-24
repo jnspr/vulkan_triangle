e3c287ef8709d618092feb5de067bf4e971a60b1
Jonas Sprenger <jnspr@outlook.de>
2023-08-24 03:50:36 +0200

    graphics.hpp: Add vector of framebuffers and define their setup function

--- graphics.hpp
+++ graphics.hpp
@@ -10,18 +10,19 @@ public:
     explicit Graphics(glfw::Window &window);
     ~Graphics();
 private:
-    glfw::Window                    &m_window;
-    vk::UniqueInstance               m_instance;
-    vk::UniqueSurfaceKHR             m_surface;
-    vk::PhysicalDevice               m_physicalDevice;
-    vk::SurfaceFormatKHR             m_surfaceFormat;
-    uint32_t                         m_queueFamilyIndex;
-    vk::UniqueDevice                 m_logicalDevice;
-    vk::Queue                        m_queue;
-    vk::Extent2D                     m_imageExtent;
-    vk::UniqueSwapchainKHR           m_swapchain;
-    vk::UniqueRenderPass             m_renderPass;
-    std::vector<vk::UniqueImageView> m_imageViews;
+    glfw::Window                      &m_window;
+    vk::UniqueInstance                 m_instance;
+    vk::UniqueSurfaceKHR               m_surface;
+    vk::PhysicalDevice                 m_physicalDevice;
+    vk::SurfaceFormatKHR               m_surfaceFormat;
+    uint32_t                           m_queueFamilyIndex;
+    vk::UniqueDevice                   m_logicalDevice;
+    vk::Queue                          m_queue;
+    vk::Extent2D                       m_imageExtent;
+    vk::UniqueSwapchainKHR             m_swapchain;
+    vk::UniqueRenderPass               m_renderPass;
+    std::vector<vk::UniqueImageView>   m_imageViews;
+    std::vector<vk::UniqueFramebuffer> m_framebuffers;
 
     // Device and presentation setup
     void selectPhysicalDevice();
@@ -29,4 +30,5 @@ private:
     void createSwapchain();
     void createRenderPass();
     void createImageViews();
+    void createFramebuffers();
 };
