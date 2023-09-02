8af10c0c8f6009510165434da48c58186c095a27
Jonas Sprenger <jnspr@outlook.de>
2023-09-02 23:02:32 +0200

    pch.hpp: Switch pragma once to define-based include guard to suppress compiler warning

--- pch.hpp
+++ pch.hpp
@@ -1,7 +1,10 @@
-#pragma once
+#ifndef PCH_hpp
+#define PCH_hpp
 
 #include <utility>
 #include <glm/glm.hpp>
 #include <vulkan/vulkan.hpp>
 #include <glfwpp/glfwpp.h>
 #include <shaderc/shaderc.hpp>
+
+#endif // PCH_hpp
